95f7d975ae470311d8c01640a5ee06b8ab5e126a
ChillibyteUK <dan@chillibyte.co.uk>
2026-03-24 13:20:18 +0000

    logos

--- blocks/cb-logo-carousel.php
+++ blocks/cb-logo-carousel.php
@@ -16,7 +16,7 @@ $block_id = $block['id'] ?? '';
 	if ( get_field( 'title' ) ) {
 		?>
 	<div class="container">
-		<h2 class="cb-logo-carousel__title has-white-color">
+		<h2 class="cb-logo-carousel__title has-white-color mb-4">
 			<?= esc_html( get_field( 'title' ) ); ?>
 		</h2>
 	</div>
