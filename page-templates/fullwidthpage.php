8559f16f404605b5262427e638bf3a3bd70c3072
ChillibyteUK <dan@chillibyte.co.uk>
2026-04-16 13:47:17 +0100

    fullwidthpage

---
+++ page-templates/fullwidthpage.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+<main class="site-main" id="main" role="main">
+	<div class="container">
+		<?php
+		while ( have_posts() ) {
+			the_post();
+			the_content();
+		}
+		?>
+	</div>
+</main>
+<?php
+get_footer();
